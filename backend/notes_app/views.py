667920124c470cdf78d76344b4cf2d51450f7882
mwz73trz <michaelzura@bellsouth.net>
2022-12-12 11:22:15 -0500

    backend complete

---
+++ backend/notes_app/views.py
@@ -0,0 +1,7 @@
+from rest_framework import viewsets
+from .serializers import NoteSerializer
+from .models import Note
+
+class NoteViewSet(viewsets.ModelViewSet):
+    queryset = Note.objects.all()
+    serializer_class = NoteSerializer
